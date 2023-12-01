470e837d394b7be2f42b62cee0a7d9e81c945889
Thandeka Sineli <Thandekasineli@gmail.com>
2023-12-01 10:55:39 +0200

    firest commit

---
+++ scripts/index.js
@@ -0,0 +1,92 @@
+
+let todoArray = JSON.parse(localStorage.getItem('Items')) || [];
+
+function saveTodoList() {
+  localStorage.setItem('Items', JSON.stringify(todoArray));
+}
+
+
+function displayTodoList() {
+  const todoList = document.getElementById('todoList');
+  let todoListHTML = '';
+
+  todoArray.forEach((item, index) => {
+    const completedClass = item.completed ? 'completed' : '';
+    const textDecoration = item.completed ? 'text-decoration: line-through;' : '';
+    todoListHTML += `
+      <li class="${completedClass}">
+        <input type="checkbox" class="checkbox" data-index="${index}" ${item.completed ? 'checked' : ''}>
+        <span class="itemName" style="${textDecoration}">${item.name}</span>
+        <span class="deleteBtn" data-index="${index}">X</span>
+      </li>
+    `;
+  });
+
+  todoList.innerHTML = todoListHTML;
+
+  const checkboxes = document.querySelectorAll('.checkbox');
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', (e) => {
+      const index = e.target.dataset.index;
+      toggleCompleted(index);
+    });
+  });
+
+  const deleteButtons = document.querySelectorAll('.deleteBtn');
+  deleteButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      const index = e.target.dataset.index;
+      removeItem(index);
+    });
+  });
+}
+
+// Function to add a new item to the todo list
+function addItem() {
+  const newEntry = document.getElementById('newEntry').value.trim();
+
+  if (newEntry !== '' && newEntry.length > 3 && /^[A-Z].*/.test(newEntry)) {
+    const newItem = {
+      id: Date.now(),
+      name: newEntry,
+      createdDate: new Date().toLocaleDateString(),
+      completed: false
+    };
+
+    todoArray.push(newItem);
+    saveTodoList();
+    displayTodoList();
+
+    // Clear input field after adding the task
+    document.getElementById('newEntry').value = '';
+  } else {
+    alert('Please enter a valid task. It should start with an uppercase letter, have more than three characters, and should not be empty.');
+  }
+}
+
+
+function toggleCompleted(index) {
+  todoArray[index].completed = !todoArray[index].completed;
+  saveTodoList();
+  displayTodoList();
+}
+
+
+function removeItem(index) {
+  todoArray.splice(index, 1);
+  saveTodoList();
+  displayTodoList();
+}
+
+function sortItems() {
+  todoArray.sort((a, b) => a.name.localeCompare(b.name));
+  saveTodoList();
+  displayTodoList();
+}
+
+document.getElementById('addBtn').addEventListener('click', addItem);
+
+document.getElementById('sortBtn').addEventListener('click', sortItems);
+
+displayTodoList();
+
